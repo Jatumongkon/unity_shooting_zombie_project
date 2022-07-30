054b6dadb1dc158a0a38100d844489785b199770
จตุมงคล แสงพุ่ม <6110110050@psu.ac.th>
2022-07-30 14:26:00 +0700

    add movement

---
+++ Unity 3D Class Project/Assets/Script/playerController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public int hp = 10;
+    public float speed = 10.0f;
+    public Vector2 moveDirection;
+
+    private float horizontal;
+    private float vertical;
+    private Animator animator; 
+    //private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        animator = gameObject.GetComponent<Animator>();
+        //rb = gameObject.GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool("Run", false);
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        moveDirection = new Vector2(horizontal, vertical);
+
+        transform.Translate(moveDirection * speed * Time.deltaTime);
+        
+        if(horizontal != 0 || vertical != 0)
+        {
+            animator.SetBool("Run", true);
+        }
+
+
+    }
+}
